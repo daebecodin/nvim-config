1f1b7333daa0ae01fc29985dd5d47bfb1f5b0db8
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-03-29 02:05:35 -0700

    atam one dark theme added

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,19 +4,26 @@
 --
 -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 return {
-  'folke/tokyonight.nvim',
+  'olimorris/onedarkpro.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
     ---@diagnostic disable-next-line: missing-fields
-    require('tokyonight').setup {
+    require('onedark').setup {
       styles = {
         comments = { italic = false }, -- Disable italics in comments
       },
+      colors = {},
     }
 
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
+    vim.cmd.colorscheme 'onedark_vivid'
   end,
 }
+
+--onedark
+--onelight
+--onedark_vivid
+--onedark_dark
+--vaporwave
