2f37a1e78a957cf4015fbe3b961b6189614c70c2
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-03-29 06:44:39 -0700

    transparent setup

--- lua/custom/plugins/transparent-nvim.lua
+++ lua/custom/plugins/transparent-nvim.lua
@@ -1,45 +1,12 @@
 return {
-
-  -- Optional, you don't have to run setup.
-  require('transparent').setup {
-    -- table: default groups
-    groups = {
-      'Normal',
-      'NormalNC',
-      'Comment',
-      'Constant',
-      'Special',
-      'Identifier',
-      'Statement',
-      'PreProc',
-      'Type',
-      'Underlined',
-      'Todo',
-      'String',
-      'Function',
-      'Conditional',
-      'Repeat',
-      'Operator',
-      'Structure',
-      'LineNr',
-      'NonText',
-      'SignColumn',
-      'CursorLine',
-      'CursorLineNr',
-      'StatusLine',
-      'StatusLineNC',
-      'EndOfBuffer',
-    },
-    -- table: additional groups that should be cleared
-    extra_groups = {
-      'NormalFloat', -- plugins which have float panel such as Lazy, Mason, LspInfo
-      'NvimTreeNormal', -- NvimTree
-    },
-    -- table: groups you don't want to clear
-    exclude_groups = {},
-    -- function: code to be executed after highlight groups are cleared
-    -- Also the user event "TransparentClear" will be triggered
-    on_clear = function() end,
-  },
-  require('transparent').clear_prefix 'NeoTree',
+  'xiyaowong/transparent.nvim',
+  config = function()
+    require('transparent').setup({
+      extra_groups = {
+        'NormalFloat',
+        'NvimTreeNormal',
+      },
+    })
+    require('transparent').clear_prefix('NeoTree')
+  end,
 }
