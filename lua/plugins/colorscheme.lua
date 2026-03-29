96dcf22385e3f9992204920673648f1dc7819675
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-03-29 02:11:42 -0700

    atam one dark theme added

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,7 +8,7 @@ return {
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
     ---@diagnostic disable-next-line: missing-fields
-    require('onedark').setup {
+    require('onedarkpro').setup {
       styles = {
         comments = { italic = false }, -- Disable italics in comments
       },
